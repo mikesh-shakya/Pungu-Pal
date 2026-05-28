ff74e86b7cb2e8448d09f9ffa7fa465c763c94f5
Sachin Shakya <sshakya219@gmail.com>
2026-05-29 03:32:36 +0530

    Initial Commit

---
+++ Pungu-Pal/Pungu_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pungu_PalApp.swift
+//  Pungu-Pal
+//
+//  Created by Mikesh on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Pungu_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
